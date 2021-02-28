a0ff62c76f417334167bbd227f999b5bd7ba38ce
fred <frederico.goncalves@soundcloud.com>
2021-02-28 23:07:04 +0100

    Fix spotless issues

--- app/src/main/java/com/example/androiddevchallenge/data/FakeDb.kt
+++ app/src/main/java/com/example/androiddevchallenge/data/FakeDb.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.data
 
 data class PuppyEntity(
@@ -1427,4 +1442,3 @@ private val puppies = mutableMapOf(
         bio = "Push individual decide job condition nor. Kid point likely community mention order man type. Most music main sometimes. Check best go open. And fine meet material hard. Season ready nation scientist again. Place color piece so after. Above notice game me. Play book bad writer.",
     )
 )
-
--- app/src/main/java/com/example/androiddevchallenge/ui/screens/PuppyScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/screens/PuppyScreen.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui.screens
 
 import androidx.compose.animation.animateContentSize
@@ -8,8 +23,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -22,10 +45,18 @@ import com.example.androiddevchallenge.R
 import com.example.androiddevchallenge.data.getPuppy
 import com.example.androiddevchallenge.data.updatePuppy
 import com.example.androiddevchallenge.domain.models.Puppy
-import com.example.androiddevchallenge.ui.components.*
-import com.example.androiddevchallenge.ui.shared.*
+import com.example.androiddevchallenge.ui.components.Bio
+import com.example.androiddevchallenge.ui.components.PetAssociation
+import com.example.androiddevchallenge.ui.components.PuppyDetailsHeader
+import com.example.androiddevchallenge.ui.components.PuppyInfo
+import com.example.androiddevchallenge.ui.components.PuppyInfoState
+import com.example.androiddevchallenge.ui.components.TopBar
+import com.example.androiddevchallenge.ui.shared.PuppyDetailsAssociationState
+import com.example.androiddevchallenge.ui.shared.PuppyDetailsBioState
+import com.example.androiddevchallenge.ui.shared.PuppyDetailsHeaderState
+import com.example.androiddevchallenge.ui.shared.PuppyDetailsInfoState
+import com.example.androiddevchallenge.ui.shared.PuppyDetailsScreenState
 import com.example.androiddevchallenge.ui.theme.Size
-
 
 @Composable
 fun PuppyScreen(
